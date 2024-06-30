65f459148d0cd168f2ce95428daa7cb0be2ae412
NO BI TA <140918070+Nobita010@users.noreply.github.com>
2024-06-30 21:17:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
-const videoUrls = [
-  "https://i.imgur.com/nhs8YG7.mp4",
-  "https://i.imgur.com/d4iHDG4.mp4"
-];
+const videoUrlsFilePath = 'video_urls.txt';
 
 app.get('/videos', (req, res) => {
-  res.json(videoUrls);
+    fs.readFile(videoUrlsFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading file:", err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        const videoUrls = data.split('\n').filter(url => url.trim() !== '');
+
+        const videoInfo = {
+            count: videoUrls.length,
+            type: "video"
+        };
+        res.json(videoInfo);
+    });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
